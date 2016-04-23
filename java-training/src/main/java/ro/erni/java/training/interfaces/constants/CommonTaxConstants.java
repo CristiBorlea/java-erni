1ec8e9cd6209709d33132679fa881ccae86e3973
RaduCazacu <cazacu23@yahoo.com>
2016-04-23 22:08:53 +0300

    Constant Interfaces

---
+++ java-training/src/main/java/ro/erni/java/training/interfaces/constants/CommonTaxConstants.java
@@ -0,0 +1,15 @@
+package ro.erni.java.training.interfaces.constants;
+
+import java.math.BigDecimal;
+
+public interface CommonTaxConstants {
+    
+    public static final String MAIN_KANTON_NAME = "Zuerich";
+    
+    public static final BigDecimal TAXATION_PERCENT = new BigDecimal("21.05");
+    
+    public static final String TAX_TYPE = "ELM";
+    
+    public static final String DOCUMENT_TYPE = "SDEC";
+
+}
---
+++ java-training/src/main/java/ro/erni/java/training/interfaces/constants/MainApp.java
@@ -0,0 +1,13 @@
+package ro.erni.java.training.interfaces.constants;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+	System.out.println(CommonTaxConstants.DOCUMENT_TYPE);
+	System.out.println(CommonTaxConstants.MAIN_KANTON_NAME);
+	System.out.println(CommonTaxConstants.TAX_TYPE);
+	System.out.println(CommonTaxConstants.TAXATION_PERCENT);
+	
+    }
+
+}
